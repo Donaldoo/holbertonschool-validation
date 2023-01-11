c387d752caa97a63e2cffc0abb4fdf3670425473
Donaldoo <105612348+Donaldoo@users.noreply.github.com>
2023-01-11 12:13:27 +0100

    update

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get install -y shellcheck snapd git make build-essential wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb && sudo rm -rf usr/bin/go
+apt-get update && apt-get install -y shellcheck make wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb

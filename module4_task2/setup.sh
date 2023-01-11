1d0ac761ed5f353474fb31baec4b5c1d356d459d
Donaldoo <105612348+Donaldoo@users.noreply.github.com>
2023-01-12 00:03:36 +0100

    test

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 apt-get update && apt-get install make wget && rm -rf usr/bin/go && wget /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64 && dpkg -i hadolint-Linux-x86_64
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test

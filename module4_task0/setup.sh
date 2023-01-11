39dcd240e52677a4138c44782e4116a397c2da91
Donaldoo <105612348+Donaldoo@users.noreply.github.com>
2023-01-11 23:22:53 +0100

    update script

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-apt-get update && apt-get install -y make wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+sudo apt-get install ./install_hugo.deb

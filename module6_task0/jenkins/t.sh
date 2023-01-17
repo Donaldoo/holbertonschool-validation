41a71f2653986e75bfa3a100c43ea1358575afb7
Donaldoo <donald.kumaraku@gmail.com>
2023-01-17 22:38:46 +0100

    update

--- module6_task0/jenkins/t.sh
+++
@@ -1,2 +0,0 @@
-JENKINS_HOST=donaldoo:donaldo123edc@localhost:8080
-curl -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/'
